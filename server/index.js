bbc6c66d95f8478549cb94ed227d41d1b26a21b1
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-13 14:02:43 -0600

    coded use of session into server file

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const sessions = require('express-session');
 
 const app = express(),
     {SERVER_PORT,
-    CONNECTION_STRING} = process.env;
+    CONNECTION_STRING,
+    SESSION_SECRET} = process.env;
+
+app.use(bodyParser.json());
+app.use(sessions({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        maxAge: 8640000000 // 100 days
+    }
+}));
 
 app.listen(SERVER_PORT, ()=>console.log(`Fitness Tracker Running on Port: ${SERVER_PORT}`))
