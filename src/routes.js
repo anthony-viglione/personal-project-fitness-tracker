1e80ff3dab6f5472bd3fcb60ad61e464d3a126cf
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-13 15:31:13 -0600

    added routes.js file and created the route for the Login component

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Login from './components/Login/Login'
+
+export default (
+    <Switch>
+        <Route exact path ='/login' component={Login} />
+    </Switch>
+)
