c63c6a0bd4aac2f50adee862887e9517544c7d35
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-13 15:22:14 -0600

    added state and state modifying function to inputs of Login component. deleted old logo svg file

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
 
 class Login extends Component{
-    // constructor(){
-    //     super()
+    constructor(){
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
 
-    // }
+    handleChange(prop,val) {
+        this.setState({
+            [prop]: val
+        })
+    }
 
     render(){
+        const{ email, password } = this.state;
+        console.log({password:password})
         return(
             <div>Login Component
                 <div>
                     <div>Email</div>
-                    <input/>
+                    <input value={email} onChange={e => this.handleChange('email', e.target.value)}/>
                 </div>
                 <div>
                     <div>Password</div>
-                    <input/>
+                    <input type='password' value={password} onChange={e => this.handleChange('password', e.target.value)}/>
                 </div>
                 <div>
                     <button>Login</button>
