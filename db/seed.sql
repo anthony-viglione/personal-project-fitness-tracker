98fffa11c467389079b71211c91739fdb7514e76
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-14 13:46:05 -0600

    added console logs to controller for testing; created db folder with seed.sql file

---
+++ db/seed.sql
@@ -0,0 +1,30 @@
+CREATE TABLE fitness_users (
+id SERIAL NOT NULL,
+password VARCHAR(255),
+email VARCHAR(50),
+username VARCHAR(30),
+user_image TEXT,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE foods (
+id SERIAL NOT NULL,
+food VARCHAR(100),
+calories INTEGER,
+fat INTEGER,
+protein INTEGER,
+carb INTEGER,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE food_history (
+id SERIAL NOT NULL,
+date VARCHAR(50), 
+repast VARCHAR(25),
+food_id INTEGER NOT NULL,
+food_quantity INTEGER,
+user_id INTEGER NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (food_id) REFERENCES foods(id),
+FOREIGN KEY (user_id) REFERENCES fitness_users(id)
+);
--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcryptjs');
 
 module.exports = {
-    register: async(req,res) => {},
-    login: async(req,res) => {},
-    getUser: (req,res) => {},
-    logout: (req,res) => {}
+    register: async(req,res) => {console.log(req)},
+    login: async(req,res) => {console.log('hit login')},
+    getUser: (req,res) => {console.log('hit getUser')},
+    logout: (req,res) => {console.log('hit logout')}
 }
