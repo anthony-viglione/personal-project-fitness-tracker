da3d8368f9d73089f8b034bc21235fe42acf0185
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-20 15:03:26 -0600

    connected Form to redux to access redux state, added a console log to the render to confirm that it was connected and viewable

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Axios from 'axios';
+
 
 class Form extends Component{
     constructor(){
@@ -7,7 +10,8 @@ class Form extends Component{
             calorieGoal:0,
             fatGoalPercent:0,
             proteinGoalPercent:0,
-            carbGoalPercent:0
+            carbGoalPercent:0,
+            button:false
         }
     }
 
@@ -17,9 +21,26 @@ class Form extends Component{
         })
     }
 
+    changeGoals = async()=>{
+        let userGoals = {
+            email: this.props.email,
+            calorieGoal:this.state.calorieGoal,
+            fatGoalPercent:this.state.fatGoalPercent,
+            proteinGoalPercent:this.state.proteinGoalPercent,
+            carbGoalPercent:this.state.carbGoalPercent
+        }
+
+        try{
+            let res = await axios
+        } catch(err) {
+            alert(`something went wrong${err}`)
+        }
+    }
+
     render(){
         const{calorieGoal, fatGoalPercent, proteinGoalPercent, carbGoalPercent} = this.state
         // console.log(calorieGoal, fatGoalPercent,proteinGoalPercent,carbGoalPercent)
+        console.log(this)
         return(
             <div>
                 <div>
@@ -46,8 +67,13 @@ class Form extends Component{
                 <div>
                     <button onClick={()=>console.log(calorieGoal,fatGoalPercent,proteinGoalPercent,carbGoalPercent)}>Check State</button>
                 </div>
+                {/* <div>{this.state.button && <button></button>}</div> */}
             </div>
         )
     }
 }
-export default Form;
+
+const mapStateToProps = reduxState => {
+    return reduxState
+}
+export default connect(mapStateToProps) (Form);
