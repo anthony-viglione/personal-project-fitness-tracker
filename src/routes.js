5501df37ba6b4881189467f8d3db7e26bd76b54a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-20 11:50:59 -0600

    added semicolon to line 5

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
 import Login from './components/Login/Login';
-import Home from './components/Home/Home'
+import Home from './components/Home/Home';
 
 export default (
     <Switch>
