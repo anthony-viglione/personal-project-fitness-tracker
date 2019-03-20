8f2c9a3037eb4bfa3881532dc3ae0df4ba17a266
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-20 14:04:40 -0600

    added state for each input as well as a handle method for all of them. features a Check State button to console log current state

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,34 +3,48 @@ import React, {Component} from 'react';
 class Form extends Component{
     constructor(){
         super()
+        this.state={
+            calorieGoal:0,
+            fatGoalPercent:0,
+            proteinGoalPercent:0,
+            carbGoalPercent:0
+        }
+    }
 
+    handleInput(prop, val) {
+        this.setState({
+            [prop]:val
+        })
     }
 
     render(){
+        const{calorieGoal, fatGoalPercent, proteinGoalPercent, carbGoalPercent} = this.state
+        // console.log(calorieGoal, fatGoalPercent,proteinGoalPercent,carbGoalPercent)
         return(
             <div>
-                <div>
-                </div>
                 <div>
                     Form Component
                 </div>
                 <div>
                     <div>
                         Calorie Goal
-                        <input/>
+                        <input value={calorieGoal} onChange={e => this.handleInput('calorieGoal', e.target.value)}/>
                     </div>
                     <div>
                         Fat Goal
-                        <input/>
+                        <input value={fatGoalPercent}  onChange={e => this.handleInput('fatGoalPercent', e.target.value)}/>
                     </div>
                     <div>
                         Protein Goal
-                        <input/>
+                        <input value={proteinGoalPercent} onChange={e => this.handleInput('proteinGoalPercent', e.target.value)}/>
                     </div>
                     <div>
                         Carb Goal
-                        <input/>
+                        <input value={carbGoalPercent} onChange={e => this.handleInput('carbGoalPercent', e.target.value)}/>
                         </div>
+                </div>
+                <div>
+                    <button onClick={()=>console.log(calorieGoal,fatGoalPercent,proteinGoalPercent,carbGoalPercent)}>Check State</button>
                 </div>
             </div>
         )
