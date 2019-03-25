ebfad74386e0066952737e5605f34c72c317755a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-25 12:46:08 -0600

    added a space

--- db/tracker/update_goals.sql
+++ db/tracker/update_goals.sql
@@ -2,4 +2,4 @@ UPDATE fitness_users
 SET calorie_goal = ${calorieGoal}, fat_goal_percent = ${fatGoalPercent}, protein_goal_percent = ${proteinGoalPercent}, carb_goal_percent = ${carbGoalPercent}
 WHERE email = ${email}
 
-returning id, email, user_image, calorie_goal, fat_goal_percent,protein_goal_percent, carb_goal_percent;
+returning id, email, user_image, calorie_goal, fat_goal_percent, protein_goal_percent, carb_goal_percent;
