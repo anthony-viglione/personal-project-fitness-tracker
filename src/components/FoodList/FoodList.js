5d023a44d386edba321c4e352c79fa6f7c73262a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-25 16:21:46 -0600

    finished functionality gets user's foods on mount, maps over the array from the database and renders each piece of data

--- src/components/FoodList/FoodList.js
+++ src/components/FoodList/FoodList.js
@@ -1,14 +1,55 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class FoodList extends Component{
-    // constructor(){
-    //     super()
+    constructor(){
+        super()
+        this.state={
+            name:'',
+            foods:""
+        }
+    }
 
-    // }
+
+    componentDidMount = async ()=> {
+        let res = await axios.get('/api/getFoods')
+        console.log(res.data)
+        let foods = res.data.map((food)=>{
+            return(
+                <div style={{dislpay:'flex', margin:'5px 5px 5px 5px'}}>
+                    <div>{food.email}</div>
+                    <div>{food.food}</div>
+                    <div>{food.calories}</div>
+                    <div>{food.carb}</div>
+                    <div>{food.protein}</div>
+                    <div>{food.fat}</div>
+                    <button>delete</button>
+                </div>
+
+            )
+        })
+        this.setState({
+            foods:foods,
+            name:res.data[0].email
+        })
+    }
 
     render(){
         return(
-            <div >List of Foods</div>
+            <div >
+                <div >List of Foods for {this.state.name}</div>
+                <div style={{display:'flex'}}>
+                    <div style={{dislpay:'flex', margin:'5px 5px 5px 5px'}}>
+                        <div>Person</div>
+                        <div>Food</div>
+                        <div>Calories</div>
+                        <div>Carb</div>
+                        <div>Protein</div>
+                        <div>Fat</div>
+                    </div>
+                    {this.state.foods}
+                </div>
+            </div>
         )
     }
 }
