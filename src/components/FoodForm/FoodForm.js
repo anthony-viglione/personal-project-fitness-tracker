ff3d5a4d264a0494054c01fa77ddadcb5140173a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-24 21:31:45 -0600

    created basic FoodForm component that renders it's name

---
+++ src/components/FoodForm/FoodForm.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class FoodForm extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                FoodForm Component
+            </div>
+        )
+    }
+}
+
+export default FoodForm;
