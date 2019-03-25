19d246c82140c6a68a3093fd4018a256b515e294
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-25 10:43:57 -0600

    created FoodList component  that renders list of foods and commented out constructor

---
+++ src/components/FoodList/FoodList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class FoodList extends Component{
+    // constructor(){
+    //     super()
+
+    // }
+
+    render(){
+        return(
+            <div >List of Foods</div>
+        )
+    }
+}
+
+export default FoodList;
