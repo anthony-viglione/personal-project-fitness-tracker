a86f6942d41a36fc73d0edb46081f6ee2dc65b5b
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-03 14:08:00 -0600

    typo correction

--- db/tracker/get_foods.sql
+++ db/tracker/get_foods.sql
@@ -1,4 +1,4 @@
 SELECT foods.id, foods.food, foods.calories, foods.fat, foods.protein, foods.carb, fu.email
 FROM foods
-join fitness_users fu on foods.user_id = fu.id
+JOIN fitness_users fu ON foods.user_id = fu.id
 WHERE user_id = ${id}
