db6e813192cfa0edd90490b15747a353a5872ae0
Anthony Viglione <viglione2014.2015@gmail.com>
2019-04-04 17:57:44 -0600

    added updateFood method to reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,7 +7,8 @@ const initialState = {
     proteinGoalPercent:0,
     carbGoalPercent:0,
     showGoalForm:false,
-    showFoodForm:false
+    showFoodForm:false,
+    foods:[]
 
 
 }
@@ -15,6 +16,7 @@ const initialState = {
 const UPDATE_USER = 'UPDATE_USER';
 const CLEAR_USER = 'CLEAR_USER';
 const UPDATE_GOALS = 'UPDATE_GOALS';
+const UPDATE_FOODS = 'UPDATE_FOODS';
 const TOGGLE_SHOW_GOAL_FORM = 'TOGGLE_SHOW_GOAL_FORM';
 const TOGGLE_SHOW_FOOD_FORM ='TOGGLE_SHOW_FOOD_FORM';
 
@@ -35,6 +37,13 @@ export function updateGoals(goals){
     return {
         type: UPDATE_GOALS,
         payload: goals
+    }
+}
+
+export function updateFoods(foods){
+    return {
+        type: UPDATE_FOODS,
+        payload: foods
     }
 }
 
@@ -59,17 +68,26 @@ export default function reducer(state = initialState, action){
             const {id, email} = payload;
             const img = payload.user_image;
             return{...state, id, email, img}
+
         case CLEAR_USER:
             return{...state, id:0, email:'', calorieGoal:0, fatGoalPercent:0, proteinGoalPercent:0, carbGoalPercent:0}
+
         case UPDATE_GOALS:
             const {calorie_goal, fat_goal_percent, protein_goal_percent, carb_goal_percent} = payload;
             return{...state, calorieGoal: calorie_goal, fatGoalPercent: fat_goal_percent, proteinGoalPercent: protein_goal_percent, carbGoalPercent: carb_goal_percent}
+            
+        case UPDATE_FOODS:
+            const{foods} = payload;
+            return{...state, foods:foods}
+
         case TOGGLE_SHOW_GOAL_FORM:
             const {showGoalForm} = payload;
             return{...state, showGoalForm: !showGoalForm}
+
         case TOGGLE_SHOW_FOOD_FORM:
             const {showFoodForm} = payload;
             return{...state, showFoodForm: !showFoodForm}
+
         default:
             return state
     }
